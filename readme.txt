7f5641939f9ff5187861146273eb2a8c3bbcf90e
Milmor <Milmor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-03 20:30:46 +0000

    v 0.2
    git-svn-id: https://plugins.svn.wordpress.org/amministrazione-trasparente/trunk@735678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,12 +3,12 @@ Contributors: Milmor
 Tags: amministrazione, trasparente, documenti, atti, spese, comuni, pa, amministrazioni, locali, pubblicazione, online, imprese, enti, obbligo, legge, comune, modulo, decreto, 14 marzo, 2013, sovvenzioni, pubblici, pubblico, marco, milesi
 Requires at least: 3.3
 Tested up to: 3.6
-Version: 0.1a
+Version: 0.2
 Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Soluzione completa per la pubblicazione online dei documenti ai sensi del D.lgs. n. 33 del 14/03/2013: pubblicità, trasparenza e diffusione di informazioni da parte delle pubbliche amministrazioni.
+Soluzione completa per la pubblicazione dei documenti ai sensi del D.lgs. n. 33 del 14/03/2013: pubblicità, trasparenza e diffusione di informazioni.
 
 
 == Description ==
@@ -17,7 +17,10 @@ ATTENZIONE | QUESTO PLUGIN E' ANCORA IN FASE ALPHA E NON FUNZIONANTE PER SITI IN
 
 Soluzione completa per la pubblicazione online dei documenti ai sensi del D.lgs. n. 33 del 14/03/2013, riguardante il riordino della disciplina degli obblighi di pubblicità, trasparenza e diffusione di informazioni da parte delle pubbliche amministrazioni, in attuazione dell’art. 1, comma 35, della legge n. 190/2012.
 
+Questo modulo offre la gestione di queste informazioni in modo semplice e veloce. In particolare, permette di gestire ogni documento come singolo articolo che, oltre a permettere il caricamento di un file, consente di aggiungere del testo a piacere.
+
 = Funzioni del Plugin =
+* Ogni documento è gestito come Articolo autonomo
 
 = Funzioni in corso di Sviluppo =
 * Possibilità di caricamento massivo delle spese (back-end)
@@ -27,14 +30,15 @@ Soluzione completa per la pubblicazione online dei documenti ai sensi del D.lgs.
 
 1. Carica il contenuto estratto nella cartella `/wp-content/plugins/`
 2. Attiva il plugin dal menu 'Plugins' in WordPress
-3. 
 
 == Screenshots ==
-
+1. Menù Laterale
+2. Pagina di inserimento Documento
 
 
 == Changelog ==
-= Versione 0.1 28/06/2013 =
+= Versione 0.1-0.2 03/07/2013 =
 * Lancio del Progetto
+* Programmazione funzionalità
 
 (!) = Aggiornamento Importante (Sicurezza/Stabilità)
--- taxonomygenerator.php
+++ taxonomygenerator.php
@@ -1,13 +1,105 @@
 <?php
 
+//Disposizioni Generali
 wp_insert_term('Programma per la Trasparenza e l\' Integrità', 'tipologie');
 wp_insert_term('Atti generali', 'tipologie');
 wp_insert_term('Oneri informativi per cittadini e imprese', 'tipologie');
 
+//Organizzazione
 wp_insert_term('Organi di indirizzo politico-amministrativo', 'tipologie');
 wp_insert_term('Sanzioni per mancata comunicazione dei dati', 'tipologie');
 wp_insert_term('Rendiconti gruppi consiliari regionali/provinciali', 'tipologie');
 wp_insert_term('Articolazione degli uffici', 'tipologie');
-wp_insert_term('Telefono e Posta elettronica', 'tipologie');
+wp_insert_term('Telefono e posta elettronica', 'tipologie');
+
+//Consulenti e Collaboratori
+wp_insert_term('Consulenti e collaboratori', 'tipologie');
+
+//Personale
+wp_insert_term('Incarichi amministrativi di vertice', 'tipologie');
+wp_insert_term('Dirigenti', 'tipologie');
+wp_insert_term('Posizioni organizzative', 'tipologie');
+wp_insert_term('Dotazione organica', 'tipologie');
+wp_insert_term('Personale non a tempo indeterminato', 'tipologie');
+wp_insert_term('Tassi di assenza', 'tipologie');
+wp_insert_term('Incarichi conferiti e autorizzati ai dipendenti', 'tipologie');
+wp_insert_term('Contrattazione collettiva', 'tipologie');
+wp_insert_term('Contrattazione integrativa', 'tipologie');
+wp_insert_term('OIV', 'tipologie');
+
+//Bandi di Concorso
+wp_insert_term('Bandi di Concorso', 'tipologie');
+
+//Performance
+wp_insert_term('Piano della Performance', 'tipologie');
+wp_insert_term('Relazione sulla Performance', 'tipologie');
+wp_insert_term('Ammontare complessivo dei premi', 'tipologie');
+wp_insert_term('Dati relativi ai premi', 'tipologie');
+wp_insert_term('Benessere organizzativo', 'tipologie');
+
+//Enti controllati
+wp_insert_term('Enti pubblici vigilati', 'tipologie');
+wp_insert_term('Società partecipate', 'tipologie');
+wp_insert_term('Enti di diritto privato controllati', 'tipologie');
+wp_insert_term('Rappresentazione grafica', 'tipologie');
+
+//Attività e procedimenti
+wp_insert_term('Dati aggregati attività amministrativa', 'tipologie');
+wp_insert_term('Tipologie di procedimento', 'tipologie');
+wp_insert_term('Monitoraggio tempi procedimentali', 'tipologie');
+wp_insert_term('Dichiarazioni sostitutive e acquisizione d\'ufficio dei dati', 'tipologie');
+
+//Provvedimenti
+wp_insert_term('Provvedimenti organi indirizzo-politico', 'tipologie');
+wp_insert_term('Provvedimenti dirigenti', 'tipologie');
+
+//Controlli sulle imprese
+wp_insert_term('Controlli sulle imprese', 'tipologie');
+
+//Bandi di gara e contratti
+wp_insert_term('Bandi di gara e contratti', 'tipologie');
+
+//Sovvenzioni, contributi, sussidi, vantaggi economici
+wp_insert_term('Criteri e modalità', 'tipologie');
+wp_insert_term('Atti di concessione', 'tipologie');
+
+//Bilanci
+wp_insert_term('Bilancio preventivo e consuntivo', 'tipologie');
+wp_insert_term('Piano degli indicatori e risultati attesi di bilancio', 'tipologie');
+
+//Beni immobili e gestione patrimonio
+wp_insert_term('Patrimonio immobiliare', 'tipologie');
+wp_insert_term('Canoni di locazione o affitto', 'tipologie');
+
+//Controlli e rilievi sull'amministrazione
+wp_insert_term('Controlli e rilievi sull\'amministrazione', 'tipologie');
+
+//Servizi erogati
+wp_insert_term('Carta dei servizi e standard di qualità', 'tipologie');
+wp_insert_term('Costi contabilizzati', 'tipologie');
+wp_insert_term('Tempi medi di erogazione dei servizi', 'tipologie');
+wp_insert_term('Liste di attesa', 'tipologie');
+
+//Pagamenti dell' amministrazione
+wp_insert_term('Indicatore di tempestività dei pagamenti', 'tipologie');
+wp_insert_term('IBAN e pagamenti informatici', 'tipologie');
+
+//Opere pubbliche
+wp_insert_term('Opere pubbliche', 'tipologie');
+
+//Pianificazione e governo del territorio
+wp_insert_term('Pianificazione e governo del territorio', 'tipologie');
+
+//Informazioni ambientali
+wp_insert_term('Informazioni ambientali', 'tipologie');
+
+//Strutture sanitarie private accreditate
+wp_insert_term('Strutture sanitarie private accreditate', 'tipologie');
+
+//Interventi straordinari e di emergenza
+wp_insert_term('Interventi straordinari e di emergenza', 'tipologie');
+
+//Altri contenuti
+wp_insert_term('Altri contenuti', 'tipologie');
 
 ?>
