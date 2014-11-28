09b0abe6be1383b6978b8d754fe079f5d3c38528
Milmor <Milmor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 18:51:53 +0000

    v4.1.7
    git-svn-id: https://plugins.svn.wordpress.org/amministrazione-trasparente/trunk@1034652 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcodes/shortcodes-php-archive.php
+++ shortcodes/shortcodes-php-archive.php
@@ -27,10 +27,3 @@ $get_at_option_showarchivedesc = get_option('at_option_showarchivedesc'); ?>
 
 
 </div>
-
-
-<details>
-<summary>Copyright 1999-2014.</summary>
-<p> - by Refsnes Data. All Rights Reserved.</p>
-<p>All content and graphics on this web site are the property of the company Refsnes Data.</p>
-</details>
